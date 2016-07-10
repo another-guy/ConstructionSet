6296ee615f4ad4d809bebe4b68c4066ee566433a
Igor Soloydenko <soloydenko@gmail.com>
2016-07-09 22:07:38 -0700

    Initial commit

---
+++ ConstructionSet.Tests/CreateTests.cs
@@ -0,0 +1,41 @@
+﻿using Xunit;
+
+namespace ConstructionSet.Tests
+{
+    public class CreateTests
+    {
+        [Fact]
+        public void TestClassWithCtorThatHasZeroArguments()
+        {
+            var result = ConstructionSet
+                .Create<ClassWithCtorThatHasZeroArguments>
+                .UsingPrivateConstructor();
+
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public void TestClassWithCtorThatHasNonZeroArguments()
+        {
+            var result = ConstructionSet
+                .Create<ClassWithCtorThatHasNonZeroArguments>
+                .UsingPrivateConstructor(new object[] { 1, "a" });
+
+            Assert.NotNull(result);
+        }
+
+        public class ClassWithCtorThatHasZeroArguments
+        {
+            private ClassWithCtorThatHasZeroArguments()
+            {
+            }
+        }
+
+        public class ClassWithCtorThatHasNonZeroArguments
+        {
+            private ClassWithCtorThatHasNonZeroArguments(int a, string b)
+            {
+            }
+        }
+    }
+}
---
+++ ConstructionSet/Create.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using System.Reflection;
+
+namespace ConstructionSet
+{
+    public static class Create<T>
+    {
+        public static T UsingPrivateConstructor(object[] parameters = null)
+        {   
+            var defaultConstructor =
+                typeof(T)
+                    .GetTypeInfo()
+                    .GetConstructors(BindingFlags.NonPublic | BindingFlags.Instance)
+                    .First();
+
+            return (T)defaultConstructor.Invoke(parameters);
+        }
+    }
+}
