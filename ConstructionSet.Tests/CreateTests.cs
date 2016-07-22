f0facb248e037d38daa226da0224d54fd70d0676
Igor Soloydenko <soloydenko@gmail.com>
2016-07-22 01:45:49 -0700

    Add best matching ctor logic

--- ConstructionSet.Tests/CreateTests.cs
+++ ConstructionSet.Tests/CreateTests.cs
@@ -7,34 +7,66 @@ namespace ConstructionSet.Tests
         [Fact]
         public void TestClassWithCtorThatHasZeroArguments()
         {
-            var result = ConstructionSet
-                .Create<ClassWithCtorThatHasZeroArguments>
+            // Arrange
+            // Act
+            var result = Create<ClassWithPrivateCtors>
                 .UsingPrivateConstructor();
 
+            // Assert
             Assert.NotNull(result);
+            Assert.Null(result.O);
+            Assert.Null(result.S);
         }
 
         [Fact]
         public void TestClassWithCtorThatHasNonZeroArguments()
         {
-            var result = ConstructionSet
-                .Create<ClassWithCtorThatHasNonZeroArguments>
-                .UsingPrivateConstructor(1, "a");
+            // Arrange
+            var s = "a";
 
+            // Act
+            var result = Create<ClassWithPrivateCtors>
+                .UsingPrivateConstructor(s);
+
+            // Assert
             Assert.NotNull(result);
+            Assert.Null(result.O);
+            Assert.Equal(s, result.S);
         }
 
-        public class ClassWithCtorThatHasZeroArguments
+        [Fact]
+        public void TestClassWithCtorThatHasNonZeroArguments2()
         {
-            private ClassWithCtorThatHasZeroArguments()
+            // Arrange
+            var o = new object();
+
+            // Act
+            var result = Create<ClassWithPrivateCtors>
+                .UsingPrivateConstructor(o);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Same(o, result.O);
+            Assert.Null(result.S);
+        }
+
+        public class ClassWithPrivateCtors
+        {
+            public string S { get; set; }
+            public object O { get; set; }
+
+            private ClassWithPrivateCtors()
             {
             }
-        }
 
-        public class ClassWithCtorThatHasNonZeroArguments
-        {
-            private ClassWithCtorThatHasNonZeroArguments(int a, string b)
+            private ClassWithPrivateCtors(string s)
             {
+                S = s;
+            }
+
+            private ClassWithPrivateCtors(object o)
+            {
+                O = o;
             }
         }
     }
--- ConstructionSet/Create.cs
+++ ConstructionSet/Create.cs
@@ -1,4 +1,6 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 
 namespace ConstructionSet
@@ -7,13 +9,63 @@ namespace ConstructionSet
     {
         public static T UsingPrivateConstructor(params object[] parameters)
         {   
-            var defaultConstructor =
+            var constructors =
                 typeof(T)
                     .GetTypeInfo()
                     .GetConstructors(BindingFlags.NonPublic | BindingFlags.Instance)
-                    .First();
+                    .ToList();
 
-            return (T)defaultConstructor.Invoke(parameters);
+            var constructor = FindCtorBySignature(constructors, parameters);
+
+            return (T)constructor.Invoke(parameters);
+        }
+
+        private static ConstructorInfo FindCtorBySignature(
+            IEnumerable<ConstructorInfo> constructors,
+            object[] parameters)
+        {
+            return parameters.Length == 0 ?
+                FindDefaultCtor(constructors) :
+                FindBestMatchingCtor(constructors, parameters);
+        }
+
+        private static ConstructorInfo FindDefaultCtor(IEnumerable<ConstructorInfo> constructors)
+        {
+            return constructors.Single(ctor => ctor.GetParameters().Length == 0);
+        }
+
+        private static ConstructorInfo FindBestMatchingCtor(
+            IEnumerable<ConstructorInfo> constructors,
+            object[] parameters)
+        {
+            var scores = constructors
+                .ToDictionary(constructor => CalculateMatchScore(constructor, parameters));
+
+            var candidateCtor = scores[scores.Keys.Max()];
+            if (candidateCtor.GetParameters().Length != parameters.Length)
+                throw new InvalidOperationException(""); // TODO What about varargs/params? TODO Unit test too
+            return candidateCtor;
+        }
+
+        private static uint CalculateMatchScore(MethodBase method, object[] parameters)
+        {
+            uint score = 0;
+            var parameterInfos = method.GetParameters();
+            if (parameterInfos.Length == parameters.Length)
+                for (var i = 0; i < parameters.Length; i++)
+                {
+                    var parameterInfoType = parameterInfos[i].ParameterType;
+                    var parameterType = parameters[i].GetType();
+
+                    if (parameterInfoType == parameterType)
+                        score += 1000;
+                    else if (parameterType.IsInstanceOfType(parameterInfoType))
+                        score += 10;
+                    else if (parameterInfoType == typeof(object))
+                        score += 1;
+                }
+
+            return score;
         }
     }
 }
