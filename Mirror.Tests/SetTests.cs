823e0ea54ac488ea85eb885b7cf4e4522235ba18
Igor Soloydenko <soloydenko@gmail.com>
2016-07-31 17:28:36 -0700

    Add privade field Set implementation

---
+++ Mirror.Tests/SetTests.cs
@@ -0,0 +1,37 @@
+﻿using Mirror.Tests.TestClasses;
+using Xunit;
+
+namespace ConstructionSet.Tests
+{
+    public class SetTests
+    {
+        private readonly ClassWithPrivateFields target = new ClassWithPrivateFields();
+        private readonly int valueToSet = 123;
+
+        [Fact]
+        public void CanSetInstanceFieldValue()
+        {
+            // Arrange
+            var fieldName = ClassWithPrivateFields.InstanceFieldName;
+
+            // Act
+            Set.FieldValue(target, fieldName, valueToSet);
+
+            // Assert
+            Assert.Equal(target.DirectInstanceField, valueToSet);
+        }
+
+        [Fact]
+        public void CanSetStaticFieldValue()
+        {
+            // Arrange
+            var fieldName = ClassWithPrivateFields.StaticFieldName;
+
+            // Act
+            Set.StaticFieldValue<ClassWithPrivateFields>(fieldName, valueToSet);
+
+            // Assert
+            Assert.Equal(ClassWithPrivateFields.DirectStaticField, valueToSet);
+        }
+    }
+}
---
+++ Mirror/Set.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+
+namespace ConstructionSet
+{
+    public static class Set
+    {
+        public static void FieldValue<T>(T target, string fieldName, object value)
+        {
+            var field = typeof(T).GetField(fieldName, BindingFlags.NonPublic | BindingFlags.Instance);
+            field.SetValue(target, value);
+        }
+
+        public static void StaticFieldValue<T>(string fieldName, object value)
+        {
+            var field = typeof(T).GetField(fieldName, BindingFlags.NonPublic | BindingFlags.Static);
+            field.SetValue(null, value);
+        }
+    }
+}
