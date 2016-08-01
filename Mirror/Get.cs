db5a8532b9ea8ad1fb841b782f2c15ff278094b1
Igor Soloydenko <soloydenko@gmail.com>
2016-07-31 17:43:22 -0700

    Refactor Get class

--- Mirror/Get.cs
+++ Mirror/Get.cs
@@ -5,28 +5,27 @@ namespace ConstructionSet
 {
     public static class Get
     {
+        private static readonly BindingFlags privateInstance = BindingFlags.NonPublic | BindingFlags.Instance;
+        private static readonly BindingFlags privateStatic = BindingFlags.NonPublic | BindingFlags.Static;
+
         public static object FieldValue<T>(T target, string fieldName)
         {
-            var field = typeof(T).GetField(fieldName, BindingFlags.NonPublic | BindingFlags.Instance);
-            return field.GetValue(target);
+            return typeof(T).GetField(fieldName, privateInstance).GetValue(target);
         }
 
         public static object StaticFieldValue<T>(string fieldName)
         {
-            var field = typeof(T).GetField(fieldName, BindingFlags.NonPublic | BindingFlags.Static);
-            return field.GetValue(null);
+            return typeof(T).GetField(fieldName, privateStatic).GetValue(null);
         }
 
         public static object PropertyValue<T>(T target, string propertyName)
         {
-            var field = typeof(T).GetProperty(propertyName, BindingFlags.NonPublic | BindingFlags.Instance);
-            return field.GetValue(target);
+            return typeof(T).GetProperty(propertyName, privateInstance).GetValue(target);
         }
 
         public static object StaticPropertyValue<T>(string propertyName)
         {
-            var field = typeof(T).GetProperty(propertyName, BindingFlags.NonPublic | BindingFlags.Static);
-            return field.GetValue(null);
+            return typeof(T).GetProperty(propertyName, privateStatic).GetValue(null);
         }
     }
 }
