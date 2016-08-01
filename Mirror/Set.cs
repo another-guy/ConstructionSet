1f78facaf4871a8a91041898e1e8910264ccf3e1
Igor Soloydenko <soloydenko@gmail.com>
2016-07-31 17:45:13 -0700

    Refactored Set class

--- Mirror/Set.cs
+++ Mirror/Set.cs
@@ -4,16 +4,17 @@ namespace ConstructionSet
 {
     public static class Set
     {
+        private static readonly BindingFlags privateInstance = BindingFlags.NonPublic | BindingFlags.Instance;
+        private static readonly BindingFlags privateStatic = BindingFlags.NonPublic | BindingFlags.Static;
+
         public static void FieldValue<T>(T target, string fieldName, object value)
         {
-            var field = typeof(T).GetField(fieldName, BindingFlags.NonPublic | BindingFlags.Instance);
-            field.SetValue(target, value);
+            typeof(T).GetField(fieldName, privateInstance).SetValue(target, value);
         }
 
         public static void StaticFieldValue<T>(string fieldName, object value)
         {
-            var field = typeof(T).GetField(fieldName, BindingFlags.NonPublic | BindingFlags.Static);
-            field.SetValue(null, value);
+            typeof(T).GetField(fieldName, privateStatic).SetValue(null, value);
         }
     }
 }
