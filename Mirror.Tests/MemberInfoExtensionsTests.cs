809bf31fb65be6808b07babca28ff8843da54ee7
Igor Soloydenko <soloydenko@gmail.com>
2017-01-05 17:23:02 -0800

    Add MemberInfoExtensions

---
+++ Mirror.Tests/MemberInfoExtensionsTests.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using Xunit;
+
+namespace Mirror.Tests
+{
+    public class MemberInfoExtensionsTests
+    {
+        [Fact]
+        public void CanExtractClassAttributeWhenPresent()
+        {
+            // Arrange
+#pragma warning disable 618
+            var target = typeof(TargetClassWithAttributes);
+#pragma warning restore 618
+
+            // Act
+            var attributes = target.GetAttributes("Mirror.Tests.DemoAttribute");
+
+            // Assert
+            Assert.NotNull(attributes.Single() as DemoAttribute);
+        }
+
+        [Fact]
+        public void CanExtractClassAttributeWhenNotPresent()
+        {
+            // Arrange
+#pragma warning disable 618
+            var target = typeof(TargetClassWithoutAttributes);
+#pragma warning restore 618
+
+            // Act
+            var attributes = target.GetAttributes("Mirror.Tests.DemoAttribute");
+
+            // Assert
+            Assert.Equal(0, attributes.Count());
+        }
+
+        [Fact]
+        public void CanExtractMultpileClassAttributesWhenPresent()
+        {
+            // Arrange
+#pragma warning disable 618
+            var target = typeof(DemoTargetClass);
+#pragma warning restore 618
+
+            // Act
+            var attributes = target.GetAttributes("Mirror.Tests.DemoAttribute");
+
+            // Assert
+            Assert.Equal(3, attributes.Count());
+        }
+
+        [Fact]
+        public void CanExtractMethodAttributeWhenPresent()
+        {
+            // Arrange
+#pragma warning disable 618
+            var target = typeof(TargetClassWithAttributes).GetMethod("TargetMethod");
+#pragma warning restore 618
+
+            // Act
+            var attributes = target.GetAttributes("Mirror.Tests.DemoAttribute");
+
+            // Assert
+            Assert.NotNull(attributes.Single() as DemoAttribute);
+        }
+
+        [Fact]
+        public void CanExtractMethodAttributeWhenNotPresent()
+        {
+            // Arrange
+#pragma warning disable 618
+            var target = typeof(TargetClassWithoutAttributes).GetMethod("TargetMethod");
+#pragma warning restore 618
+
+            // Act
+            var attributes = target.GetAttributes("Mirror.Tests.DemoAttribute");
+
+            // Assert
+            Assert.Equal(0, attributes.Count());
+        }
+        
+        [Fact]
+        public void CanExtractMultpileMethodAttributesWhenPresent()
+        {
+            // Arrange
+#pragma warning disable 618
+            var target = typeof(DemoTargetClass).GetMethod("TargetMethod");
+#pragma warning restore 618
+
+            // Act
+            var attributes = target.GetAttributes("Mirror.Tests.DemoAttribute");
+
+            // Assert
+            Assert.Equal(2, attributes.Count());
+        }
+    }
+
+    [Demo]
+    public class TargetClassWithAttributes
+    {
+        [Demo]
+        public void TargetMethod() { }
+    }
+    
+    public class TargetClassWithoutAttributes
+    {
+        public void TargetMethod() { }
+    }
+    
+    [Demo]
+    [Demo]
+    [Demo]
+    public class DemoTargetClass
+    {
+        [Demo]
+        [Demo]
+        public void TargetMethod() { }
+    }
+
+    [AttributeUsage(AttributeTargets.All, AllowMultiple = true)]
+    public class DemoAttribute : Attribute { }
+}
---
+++ Mirror/Reflection/MemberInfoExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Reflection;
+
+namespace System
+{
+    public static class MemberInfoExtensions
+    {
+        [Pure]
+        public static IEnumerable<Attribute> GetAttributes<T>(
+            this MemberInfo memberInfo,
+            bool inherit = true)
+            where T : Attribute
+        {
+            return memberInfo
+                .GetAttributes(typeof(T).FullName, inherit);
+        }
+
+        [Pure]
+        public static IEnumerable<Attribute> GetAttributes(
+            this Type type,
+            string attributeType,
+            bool inherit = true)
+        {
+            return type
+                .GetTypeInfo()
+                .GetAttributes(attributeType, inherit);
+        }
+
+        [Pure]
+        public static IEnumerable<Attribute> GetAttributes(
+            this MemberInfo memberInfo,
+            string attributeTypeFullName,
+            bool inherit = true)
+        {
+            return memberInfo
+                .GetCustomAttributes(inherit)
+                .Where(attributeType => attributeType.GetType().FullName == attributeTypeFullName);
+        }
+    }
+}
